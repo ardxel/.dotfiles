19467ce9cc61b76751719ff2dfe191335a9fdaf3
ardxel <ardxel@gmail.com>
2024-12-22 23:47:51 +0300

    ♻️  refactor(nvim.cmp): refactor config

--- .config/nvim/lua/utils/configs/cmp.lua
+++ .config/nvim/lua/utils/configs/cmp.lua
@@ -2,12 +2,12 @@ local M = {}
 
 M.setup = function()
 	local cmp = require("cmp")
-	local lspkind = require("lspkind")
 	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
 	cmp.setup({
 		completion = {
-			completeopt = "menu,menuone",
+			completeopt = "menu,menuone,noinsert",
 		},
 		window = {
 			keyword_length = 2,
@@ -17,36 +17,59 @@ M.setup = function()
 		mapping = cmp.mapping.preset.insert({
 			["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 			["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-			["<C-k>"] = cmp.mapping.select_prev_item(),
-			["<C-j>"] = cmp.mapping.select_next_item(),
-			["<Enter>"] = cmp.mapping.confirm({
+			["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+			["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+			["<C-CR>"] = cmp.mapping.confirm({ select = true }),
+			["<CR>"] = cmp.mapping.confirm({
 				select = true,
 				behavior = cmp.ConfirmBehavior.Replace,
 			}),
-			["<Esc>"] = cmp.mapping.close(),
+			-- ["<Esc>"] = cmp.mapping.close(),
+			["<Esc>"] = function(fallback)
+				cmp.abort()
+				fallback()
+			end,
 			["<C-l>"] = cmp.mapping.complete(),
 		}),
 		sources = cmp.config.sources({
-			{ name = "nvim_lsp" },
-			{ name = "path", keyword_length = 3 },
 			{ name = "lazydev" },
+			{ name = "nvim_lsp" },
+			{ name = "path", keyword_length = 2 },
 			{ name = "buffer" },
+			{ name = "git" },
+			{ name = "snippets" },
 			{ name = "codeium" },
 		}),
 		---@diagnostic disable-next-line: missing-fields
 		formatting = {
 			fields = { "abbr", "menu", "kind" },
-			format = lspkind.cmp_format({
-				mode = "symbol_text",
-				menu = {
+			format = function(entry, item)
+				local icons = require("config.icons").kinds
+				item.kind = string.format("%s %s", icons[item.kind], item.kind)
+
+				item.menu = ({
 					nvim_lsp = "[LSP]",
 					nvim_lua = "[LUA]",
 					buffer = "[BUF]",
 					path = "[PATH]",
-				},
-				symbol_map = { Codeium = "" },
-				before = require("tailwind-tools.cmp").lspkind_format,
-			}),
+				})[entry.source.name]
+
+				local widths = {
+					abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+					menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+				}
+
+				for key, width in pairs(widths) do
+					if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+						item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+					end
+				end
+
+				return item
+				-- return lspkind.cmp_format()(entry, item)
+			end,
+			symbol_map = { Codeium = "" },
+			before = require("tailwind-tools.cmp").lspkind_format,
 		},
 	})
 
